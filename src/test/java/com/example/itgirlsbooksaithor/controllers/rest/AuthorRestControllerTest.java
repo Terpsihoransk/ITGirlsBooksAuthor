980bccdf65882296f4e83e809408341b2128d7b6
TerpsihoraNSK <arsenjeva.ea@gmail.com>
2023-05-05 19:09:39 +0700

    fix tests

--- src/test/java/com/example/itgirlsbooksaithor/controllers/rest/AuthorRestControllerTest.java
+++ src/test/java/com/example/itgirlsbooksaithor/controllers/rest/AuthorRestControllerTest.java
@@ -9,8 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false) // отключаем аутентификацию
