054f6158b4f78c9fa90a56643868f04b54ab35b7
arsenevaea <arsenevaea@gmail.com>
2023-10-29 12:56:57 +0700

    changelogs fix

--- src/main/java/com/example/itgirlsbooksaithor/models/User.java
+++ src/main/java/com/example/itgirlsbooksaithor/models/User.java
@@ -1,11 +1,11 @@
 package com.example.itgirlsbooksaithor.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import jakarta.persistence.*;
 
 @Entity
 @Data
--- src/main/java/com/example/itgirlsbooksaithor/services/UserDetailsServiceImpl.java
+++ src/main/java/com/example/itgirlsbooksaithor/services/UserDetailsServiceImpl.java
@@ -1,34 +1,34 @@
-package com.example.itgirlsbooksaithor.services;
-
-import com.example.itgirlsbooksaithor.models.User;
-import com.example.itgirlsbooksaithor.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(username));
-
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-
-            return org.springframework.security.core.userdetails.User.builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .build();
-        } else {
-            throw new UsernameNotFoundException("Не найден пользователь с логином: " + username);
-        }
-    }
-}
+//package com.example.itgirlsbooksaithor.services;
+//
+//import com.example.itgirlsbooksaithor.models.User;
+//import com.example.itgirlsbooksaithor.repositories.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//    private UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(username));
+//
+//        if (userOptional.isPresent()) {
+//            User user = userOptional.get();
+//
+//            return org.springframework.security.core.userdetails.User.builder()
+//                    .username(user.getUsername())
+//                    .password(user.getPassword())
+//                    .build();
+//        } else {
+//            throw new UsernameNotFoundException("Не найден пользователь с логином: " + username);
+//        }
+//    }
+//}
